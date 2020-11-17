2fedf0a4803614ff6ec6cb19064bde32825b8bb8
Olivier Roques <olivier@oroques.dev>
2020-11-17 22:21:20 +0100

    Restructure project

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sshcontroller-oroques",  # Replace with your own username
+    version="1.0",
+    author="Olivier Roques",
+    author_email="olivier@oroques.dev",
+    description="A package to easily run SSH commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ojroques/python-sshcontroller",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+	"Topic :: Internet",
+	"Topic :: Security :: Cryptography",
+	"Topic :: Software Development",
+    ],
+    python_requires='>=3.6',
+)
