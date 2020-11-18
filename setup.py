983738efcaa081c6c876ef63d4c2f227afbd8c97
Olivier Roques <oroques@cisco.com>
2020-11-18 13:24:32 +0100

    Update README.md and setup script

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
         "Operating System :: OS Independent",
-	"Topic :: Internet",
-	"Topic :: Security :: Cryptography",
-	"Topic :: Software Development",
+        "Topic :: Internet",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development",
     ],
     python_requires='>=3.6',
 )
